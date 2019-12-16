61c3402225453ebfb2e3eb2ad0a44657073a6afc
Ryan <rwahl005@west-mec.org>
2019-12-15 22:16:18 -0700

    Finished Level 1
    Finished the first level.

---
+++ Assets/Scripts/Game Objects/FBLABook.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FBLABook : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Rotate(new Vector3(70 * Time.deltaTime, 70 * Time.deltaTime, 0));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            gameObject.SetActive(false);
+            Debug.Log("TODO: Give this thing a purpose.");
+        }
+    }
+}
