6b79b6bd25482cac5beadc202f1e94d8dc1a2aa6
Ryan <rwahl005@west-mec.org>
2020-01-12 22:17:02 -0700

    Began Work on More Level
    Began work on two more levels.

---
+++ Assets/Levels/Forest/BackgroundFollowCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundFollowCamera : MonoBehaviour
+{
+
+    public float xOffset;
+    public float yOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position =  new Vector3(Camera.main.transform.position.x + xOffset, Camera.main.transform.position.y + yOffset, 0);
+    }
+}
