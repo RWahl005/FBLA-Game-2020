08d6c875e2813855205ab41d51112062e46f623b
Ryan <rwahl005@west-mec.org>
2020-01-13 23:41:05 -0700

    Worked On Second Level
    Continued work on the second level.

---
+++ Assets/LogMove.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogMove : MonoBehaviour
+{
+    public bool sample;
+
+    public float time;
+    private float calctime;
+    private float height = -3.90f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        calctime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!sample)
+        {
+            if(transform.position.y < height)
+            {
+                transform.position += new Vector3(0, 0.5f * Time.deltaTime, 0);
+                return;
+            }
+            transform.position += new Vector3(-1*Time.deltaTime, 0, 0);
+            calctime += Time.deltaTime;
+            if(calctime > time)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+        }
+    }
+}
---
+++ Assets/LogSpanwer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogSpanwer : MonoBehaviour
+{
+    public float cooldown;
+    public GameObject logInstance;
+    private float cooldownTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cooldownTimer -= Time.deltaTime;
+        if (cooldownTimer < 0)
+        {
+            GameObject left = Instantiate(logInstance);
+            left.transform.position = transform.position + new Vector3(0, 0, 0);
+            left.GetComponent<LogMove>().sample = false;
+            cooldownTimer = cooldown;
+        }
+    }
+}
