2b009fa5b5a93a26c841293b074c0676205ee3b7
Ryan <rwahl005@west-mec.org>
2020-01-15 22:23:15 -0700

    Third Level Completion
    The third level is complete.

---
+++ Assets/Car.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    public bool sample;
+    public float speed;
+    public float time;
+
+    private float curTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        curTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sample) return;
+        curTime += Time.deltaTime;
+        if(curTime > time)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+    }
+}
---
+++ Assets/CarSpawn.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSpawn : MonoBehaviour
+{
+    public float carTime;
+    public float carSpeed;
+    public float spawnTime;
+
+    public GameObject redCar;
+    public GameObject greenCar;
+    public GameObject blueCar;
+
+    private float time;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if(time > spawnTime)
+        {
+            time = 0;
+            int randomNum = Mathf.RoundToInt(Random.Range(0, 3));
+            GameObject newCar;
+            switch (randomNum)
+            {
+                case 0:
+                    newCar = Instantiate(redCar);
+                    break;
+                case 1:
+                    newCar = Instantiate(blueCar);
+                    break;
+                case 2:
+                    newCar = Instantiate(greenCar);
+                    break;
+                default:
+                    newCar = Instantiate(redCar);
+                    break;
+            }
+            Car cs = newCar.GetComponent<Car>();
+            cs.time = carTime;
+            cs.speed = carSpeed;
+            cs.sample = false;
+            newCar.transform.position = transform.position;
+        }
+    }
+}
